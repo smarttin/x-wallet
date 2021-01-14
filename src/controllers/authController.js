6875f3192d1d828b68306b2e897341d1d9066684
smarttin <smarttin77@gmail.com>
2021-01-14 16:45:48 +0100

    adds protectRoute action to give access to only authenticated users

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -90,4 +90,31 @@ const signin = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
-export default {signup, signin};
+const protectRoute = catchAsync(async (req, res, next) => {
+  // Get token and check of it's there
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to get access.', 401));
+  }
+
+  // Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(new AppError('The user with this token does no longer exist.', 401));
+  }
+
+  // grant access to protected route
+  req.user = currentUser;
+  next();
+});
+
+export default {signup, signin, protectRoute};
