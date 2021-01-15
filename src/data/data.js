a31442749f9e3fe62e43aa332e7601cc79347481
smarttin <smarttin77@gmail.com>
2021-01-15 17:12:02 +0100

    adds dummy-data and seeder script

---
+++ src/data/data.js
@@ -0,0 +1,61 @@
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+
+const adminId = mongoose.Types.ObjectId();
+const eliteId = mongoose.Types.ObjectId();
+const noobId = mongoose.Types.ObjectId();
+
+const eliteWalletId = mongoose.Types.ObjectId();
+const noobWalletId = mongoose.Types.ObjectId();
+
+const testPassword = bcrypt.hashSync('121212', 12);
+
+const users = [
+  {
+    _id: adminId,
+    username: 'admin',
+    email: 'admin@gmail.com',
+    password: testPassword,
+    userType: 'admin',
+    baseCurrency: null,
+    wallet: null,
+    hasWallet: false,
+  },
+  {
+    _id: eliteId,
+    username: 'elite',
+    email: 'elite@gmail.com',
+    password: testPassword,
+    userType: 'elite',
+    baseCurrency: 'CAD',
+    hasWallet: true,
+    wallet: [eliteWalletId],
+  },
+  {
+    _id: noobId,
+    username: 'nooob',
+    email: 'noob@gmail.com',
+    password: testPassword,
+    userType: 'noob',
+    baseCurrency: 'EUR',
+    hasWallet: true,
+    wallet: [noobWalletId],
+  },
+];
+
+const wallets = [
+  {
+    _id: eliteWalletId,
+    currencyName: 'CAD',
+    currencySymbol: 'CAD',
+    owner: eliteId,
+  },
+  {
+    _id: noobWalletId,
+    currencyName: 'EUR',
+    currencySymbol: 'EUR',
+    owner: noobId,
+  },
+];
+
+export {users, wallets};
---
+++ src/seeder.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/database.js';
+import Wallet from './models/walletModel.js';
+import User from './models/userModel.js';
+import {users, wallets} from './data/data.js';
+
+dotenv.config();
+connectDB();
+
+const importData = async () => {
+  try {
+    await User.deleteMany();
+    await Wallet.deleteMany();
+
+    await User.insertMany(users);
+    await Wallet.insertMany(wallets);
+
+    console.log('Data Imported!'.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await User.deleteMany();
+    await Wallet.deleteMany();
+
+    console.log('Data Deleted!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '-import') {
+  importData();
+} else if (process.argv[2] === '-delete') {
+  destroyData();
+}
